829d70cf130047c1f0dc5f285acfc7581783271a
WilkerB-12 <wilkerblanco18@gmail.com>
2022-01-24 01:53:19 +0000

    primera entrega

---
+++ src/js/icons.js
@@ -0,0 +1,9 @@
+import fontawesome from "@fortawsome/fontawesome";
+
+import faClock from "@fortawesome/fontawesome-free-regular/faClock";
+
+fontawesome.config = {
+	autoReplaceSvg: "nest",
+};
+
+fontawesome.library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,49 @@
 //import react into the bundle
+import PropTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+//import "./icons.js";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i class="far fa-clock"></i>
+			</div>
+			<div className="fourth">{props.digitFour}</div>
+			<div className="third">{props.digitThree}</div>
+			<div className="second">{props.digitTwo}</div>
+			<div className="first">{props.digitOne}</div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+	digitOne: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitFour: PropTypes.number,
+};
+
+let counter = 0;
+setInterval(function () {
+	const four = Math.floor(counter / 1000) % 10;
+	const three = Math.floor(counter / 100) % 10;
+	const two = Math.floor(counter / 10) % 10;
+	const one = Math.floor(counter / 1) % 10;
+	counter++;
+
+	//render your react application
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
